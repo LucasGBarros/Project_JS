5b30c4d3d71a058218eb77975444372b9ccf57f8
LucasGBarros <lucasgomesdebarros99@gmail.com>
2023-10-18 08:16:31 -0300

    first commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+console.log("Meu Primeiro Programa");
+
+
+// ------------------ Declaração de Variável ------------------
+
+console.log("------------------ Trabalhando com Variaveis ------------------");
+const idade = 24;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+4;
+console.log(idadeSomada)
