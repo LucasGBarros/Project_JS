a9de6d7abc2546e6984244594c37fd793929f1b5
LucasGBarros <lucasgomesdebarros99@gmail.com>
2023-10-25 16:02:15 -0300

    Atribuição de Variaveis

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,22 @@
+console.log("Trabalhando com Atribuição");
+
+const idade = 29;
+const nome = "Lucas";
+// let nome = "Lucas"; // Variavel LET permite a alterações de valores recebido das variaveis 
+const sobrenome = "Barros";
+
+console.log(nome + sobrenome);
+console.log(nome, sobrenome);
+console.log(nome + " " + sobrenome);
+
+console.log(`Meu nome é ${nome} ${sobrenome}`); // Formatação de texto ${}
+
+const nomeCompleto = nome + sobrenome;
+
+console.log(`Meu nome é ${nome}`);
+
+// Utilizando Varivel let
+let contador = 0
+contador = contador + 1
+
+console.log(contador)
