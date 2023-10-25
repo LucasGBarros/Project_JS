b53cf9170024cd531bce9722be289071094377b1
LucasGBarros <lucasgomesdebarros99@gmail.com>
2023-10-25 15:40:39 -0300

    Conversão de Tipos

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,10 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2023);
+console.log("2"+"2");
+console.log(parseInt("2") + parseInt("2"));  // transformando um valor Str em Int
+
+console.log("10" / "2");
+console.log("Lucas" / "2"); // Not a Number
+console.log(6.5); // é um número
+console.log(6,5); // , se refere a um caractere especial
