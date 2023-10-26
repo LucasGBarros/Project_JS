8a144788ab70029e66cd89cca8a41432aa023b81
LucasGBarros <Lucasgomesdebarros99@gmail.com>
2023-10-25 21:47:33 -0300

    Inserindo Condicionais

---
+++ 7-condicionais.js
@@ -0,0 +1,31 @@
+console.log(`Trabalhando com Condicionais`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+
+
+const idadeComprador = 17;
+console.log("Destinos possíveis");
+console.log(listaDeDestinos);
+
+if(idadeComprador >= 18){
+    console.log("Comprador Maior de Idade")
+    // REMOVENDO ELEMENTOS
+    listaDeDestinos.splice(1,1); 
+}
+
+else {
+    console.log("Não é Maior de idade e nao posso vender");
+}
+
+// listaDeDestinos.splice(1,1); 
+console.log(listaDeDestinos);
+
+console.log(idadeComprador >  18);
+console.log(idadeComprador >  18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador >  18);
+console.log(idadeComprador == 18);
