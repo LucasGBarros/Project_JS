2d6f82b8d3d10158315f57f1622073c1b74eced6
LucasGBarros <Lucasgomesdebarros99@gmail.com>
2023-11-22 16:36:48 -0300

    Trabalhando com condições e valores dentro da classe e operacoes

--- 2-javaScript__Programando_Orientacao_Objeto/index.js
+++ 2-javaScript__Programando_Orientacao_Objeto/index.js
@@ -1,25 +1,43 @@
 class Cliente{
 nome;
 cpf;
-agencia;
-saldo;
-rg;
+}
+class contaCorrente{
+    agencia;
+    _saldo;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-
 cliente1.nome = "Lucas";
 cliente1.cpf = 12345678901;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice"
 cliente2.cpf = 77788899945;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+// contaCorrenteLucas.depositar(-1); // conforme a descrição de classe da função depositar só recebe valor>0, o valor negativo nao é executado
 
-console.log(cliente1, cliente2)
+const contaCorrenteLucas = new contaCorrente();
+contaCorrenteLucas.agencia = 1001;
 
+contaCorrenteLucas.depositar(100);
+contaCorrenteLucas.depositar(100);
+contaCorrenteLucas.depositar(100);
+
+const valorSacado = contaCorrenteLucas.sacar(50);
+
+console.log(valorSacado);
+console.log(contaCorrenteLucas);
