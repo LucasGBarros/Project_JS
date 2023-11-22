a3b3e6fb1912edfa4f167a44b629f0be3469775f
Lucas_Barros <lucas.barros@i.systems.com.br>
2023-11-22 17:58:48 -0300

    iniciando organização de modulo e estrutura package.json

--- 2-javaScript__Programando_Orientacao_Objeto/index.js
+++ 2-javaScript__Programando_Orientacao_Objeto/index.js
@@ -1,29 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class contaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
-    }
-}
-// A classe é um modelo de estrutura para ser preenchido de acordo com as informações que voce quer
-
+import {Cliente} from "./Cliente.js"
+import {contaCorrente} from "./ContaCorrente.js"
 
 // Criando variavel com as informações da classe
 const cliente1 = new Cliente(); 
