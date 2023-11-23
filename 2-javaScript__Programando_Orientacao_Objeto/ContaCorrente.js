3a7b527a45b6041317f897e8ef18a12b5239b9f4
LucasGBarros <lucasgomesdebarros99@gmail.com>
2023-11-23 14:35:25 -0300

    Inserindo Get e Set

--- 2-javaScript__Programando_Orientacao_Objeto/ContaCorrente.js
+++ 2-javaScript__Programando_Orientacao_Objeto/ContaCorrente.js
@@ -1,7 +1,26 @@
+import { Cliente } from "./Cliente.js";
+
 export class contaCorrente{
     agencia;
+    _cliente;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
     _saldo = 0;
 
+    get saldo(){
+        return this._saldo
+    }
+
+    atribuirCliente(){}
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
@@ -20,7 +39,7 @@ export class contaCorrente{
     transferir(valor, conta){
         const valorSacado = this.sacar(valor)
         conta.depositar(valorSacado)
-        conta.cidade = "São Paulo";
+        // conta.cidade = "São Paulo";
     }
 }
 // A classe é um modelo de estrutura para ser preenchido de acordo com as informações que voce quer
