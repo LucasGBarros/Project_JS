c98ee995e2a2b686108c422ac0eb5321032575eb
LucasGBarros <lucasgomesdebarros99@gmail.com>
2023-11-23 14:36:00 -0300

    controle de dados com encapsulamento de classe

--- 2-javaScript__Programando_Orientacao_Objeto/index.js
+++ 2-javaScript__Programando_Orientacao_Objeto/index.js
@@ -7,9 +7,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Lucas";
 cliente1.cpf = 12345678901;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice"
-cliente2.cpf = 77788899945;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alice"
+// cliente2.cpf = 77788899945;
 // contaCorrenteLucas.depositar(-1); 
 // conforme a descrição de classe da função depositar só recebe valor>0, o valor negativo nao é executado
 
@@ -19,16 +19,13 @@ contaCorrenteLucas.cliente = cliente1;
 contaCorrenteLucas.depositar(500);
 
 const conta2 = new contaCorrente();
-conta2.cliente= cliente2;
+conta2.cliente= cliente1;
 conta2.agencia= 102;
-console.log(contaCorrenteLucas);
-console.log(conta2);
 
 
 let valor = 200;
 contaCorrenteLucas.transferir(valor, conta2)
 
-console.log("Valor: ", valor)
-
-console.log(contaCorrenteLucas);
-console.log(conta2);
+// conta2.saldo = 30000;
+// console.log(contaCorrenteLucas);
+console.log(conta2.saldo);
